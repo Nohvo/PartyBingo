1bb2ada1faa2f09ea35d1d7b2b9f4cedb78c474e
Nohvo <niko.ohvelo@gmail.com>
2022-04-25 02:33:28 +0300

    render grid right

--- src/screens/BingoGrid.tsx
+++ src/screens/BingoGrid.tsx
@@ -4,6 +4,7 @@ import { View, StyleSheet, Text } from 'react-native'
 import { useSelector } from 'react-redux'
 import { GridContainer } from '../features/grid'
 import { Picker } from '@react-native-picker/picker'
+import { ScrollView } from 'react-native-gesture-handler'
 
 type Props = {
     grids: GridContainer[]
@@ -16,11 +17,12 @@ const BingoGrid = (props: Props & StackScreenProps<any>) => {
     const openMenu = () => setVisible(true);
     const closeMenu = () => setVisible(false);
     // For example a grid of 9 will lead to 3x3 grid
-    const gridSize = Math.ceil(Math.sqrt(items.length))
+    const gridSize = Math.ceil(Math.sqrt(items.grid.length))
+    console.log("GRID SIZE", gridSize)
     useEffect(() => {
         props.navigation.setOptions({
             header: () => <>
-            
+
             </>
         }
         )
@@ -29,42 +31,38 @@ const BingoGrid = (props: Props & StackScreenProps<any>) => {
 
     const renderGrid = () => {
         var boxes: any = []
-        for (let i = 0; i < gridSize; i++) {
-            boxes.push(<View style={styles.square}><Text>{items[i].text}</Text></View>)
+        for (let i = 0; i < Math.sqrt(selectedGrid.grid.length); i++) {
+            boxes.push(
+                <View style={{ flexDirection: "column", flex: 1 }}>
+                    <View style={{ flexDirection: "row" }}>
+                        {renderRow(i)}
+                    </View>
+                </View>
+            )
         }
         return boxes
     }
-    const renderRow = () => {
-        for (let i = 0; i < gridSize; i++) {
-
+    const renderRow = (index) => {
+        var boxes = []
+        for (let i = 0; i < Math.sqrt(selectedGrid.grid.length); i++) {
+           boxes.push( <View style={{ flex: 1 }}>
+                <Text>{selectedGrid.grid[i + (Math.sqrt(selectedGrid.grid.length) * index)].text}</Text>
+            </View>)
         }
+        return boxes;
     }
 
     return (<>
-{/*         <View style={{ flexDirection: "row", height: `${100 / gridSize}%` }}>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-        </View>
-        <View style={{ flexDirection: "row", height: `${100 / gridSize}%` }}>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-        </View>
-        <View style={{ flexDirection: "row", height: `${100 / gridSize}%` }}>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-            <View style={styles.square}><Text>Hello</Text></View>
-        </View> */}
+
         {/*@ts-ignore*/}
-        <Picker selectedValue={selectedGrid} onValueChange={(value:GridContainer) => setSelectedGrid(value)} >
-                    {props.route.params.grids.map((grid) => {
-                        return (
-                            <Picker.Item label={grid.name} value={grid} />
-                        )
-                    })}
-                </Picker>
-        <Text>{selectedGrid.name}</Text>
+        <Picker selectedValue={selectedGrid} onValueChange={(value: GridContainer) => setSelectedGrid(value)} >
+            {props.route.params.grids.map((grid) => {
+                return (
+                    <Picker.Item label={grid.name} value={grid} />
+                )
+            })}
+        </Picker>
+        {renderGrid()}
     </>)
 }
 
